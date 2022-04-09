65620453944599992111942c7b8ce951bf83b379
CASTILLO GARCIA Carlos <carlos.castillo2@soprasteria.com>
2022-04-09 08:55:40 +0200

    Commit inicial, con modificacion de las dependencias

---
+++ src/main/java/com/app/GestionInmuebles/GestionInmueblesApplication.java
@@ -0,0 +1,13 @@
+package com.app.GestionInmuebles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionInmueblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionInmueblesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/GestionInmuebles/GestionInmueblesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.GestionInmuebles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionInmueblesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
